ac90aedde96b395d6b542499b23c44c11fd6bdac
meriadec <meriadec.pillet@gmail.com>
2018-04-28 19:40:55 +0200

    Init ledger-core bindings for NodeJs

---
+++ binding.gyp
@@ -0,0 +1,55 @@
+{
+  'variables': {
+    'core_library%': "lib",
+      'run_path%': "lib",
+      'source_path%': "src",
+  },
+  'targets': [
+    {
+      'target_name': 'ledgercore',
+      'sources': ["<!@(python glob.py <@(source_path) *.cpp *.hpp)"],
+      'include_dirs': [
+        "<!(node -e \"require('nan')\")",
+        "<!(pwd)/include",
+      ],
+      'all_dependent_settings': {
+        'include_dirs': ["<!(node -e \"require('nan')\")"],
+      },
+      'libraries': [
+        '-Wl,-rpath,<!(pwd)/<@(run_path)',
+        '-L<!(pwd)/<@(core_library)',
+        '-lledger-core'
+      ],
+      'conditions': [
+        ['OS=="mac"', {
+          'LDFLAGS': [
+            '-framework IOKit',
+            '-framework CoreFoundation',
+            '-DYLD_LIBRARY_PATH '
+          ],
+          'xcode_settings': {
+            'GCC_ENABLE_CPP_EXCEPTIONS': 'YES',
+            'OTHER_CFLAGS': [
+              '-ObjC++',
+              '-std=c++14',
+            ],
+            'OTHER_LDFLAGS': [
+              '-framework IOKit',
+              '-framework CoreFoundation'
+            ],
+          },
+        }
+      ],
+      [
+        'OS=="win"', {
+          'GCC_ENABLE_CPP_EXCEPTIONS': 'YES',
+          'OTHER_CFLAGS': ['-std=c++14']
+        }
+      ]
+    ],
+    'cflags!': ['-ansi', '-fno-exceptions'],
+    'cflags_cc!': ['-fno-exceptions'],
+    'cflags': ['-g', '-exceptions'],
+    'cflags_cc': ['-g', '-exceptions']
+  }]
+}
---
+++ signTransaction.js
@@ -0,0 +1,88 @@
+const {
+  stringToBytesArray,
+  bytesToHex,
+  createBitcoinLikeHelper
+} = require("./helpers");
+
+async function signTransaction(hwApp, transaction, isSegwitSupported = true) {
+  const rawInputs = transaction.getInputs();
+
+  // const s = transaction.serialize();
+  // const bitcoinLikeHelper = createBitcoinLikeHelper();
+
+  const inputs = await Promise.all(
+    rawInputs.map(async input => {
+      // previous transaction
+      const rawPreviousTransactionHash = await input.getPreviousTxHash();
+      const rawPreviousTransaction = await input.getPreviousTransaction();
+      const hexPreviousTransaction = bytesToHex(rawPreviousTransaction);
+      const previousTransaction = hwApp.splitTransaction(
+        hexPreviousTransaction,
+        isSegwitSupported
+      );
+
+      // output index
+      const outputIndex = input.getPreviousOutputIndex();
+
+      // sequence
+      const sequence = input.getSequence();
+
+      return [
+        previousTransaction,
+        outputIndex,
+        // we don't use that
+        // TODO: document
+        null,
+        sequence
+      ];
+    })
+  );
+
+  const associatedKeysets = rawInputs.map(input => {
+    const derivationPaths = input.getDerivationPath();
+    return derivationPaths[0].toString();
+  });
+
+  const outputs = transaction.getOutputs();
+  const output = outputs.find((output, i) => {
+    // FIXME: remove that when we get the fix (by khalil: "add method `isNull`
+    // to check if C++ implementation is null")
+    try {
+      const derivationPath = output.getDerivationPath();
+      const strDerivationPath = derivationPath.toString();
+      const derivationArr = strDerivationPath.split("/");
+      return derivationArr[derivationArr.length - 2] === "1";
+    } catch (err) {
+      return false;
+    }
+  });
+
+  const changePath = output.getDerivationPath().toString();
+  console.log(changePath);
+
+  // TODO: serialize transaction here, and cut it to get outputScript
+  const outputScriptHex = transaction
+    .getOutputs()
+    .map(output => bytesToHex(output.getScript()))
+    .join("");
+
+  console.log(`outputScriptHex:`);
+  console.log(`${outputScriptHex}`);
+
+  // TODO: detect it with address
+  const segwit = false;
+
+  const lockTime = transaction.getLockTime();
+  const initialTimestamp = transaction.getTimestamp();
+
+  // return;
+  const something = await hwApp.createPaymentTransactionNew(
+    inputs,
+    associatedKeysets,
+    changePath,
+    outputScriptHex,
+    lockTime
+  );
+}
+
+module.exports = signTransaction;
