e8b7a8e5a7cd8b075424603dde696af2006a1021
meriadec <meriadec.pillet@gmail.com>
2018-05-11 10:26:44 +0200

    detect os and download correct dylib

--- preinstall.py
+++ preinstall.py
@@ -1,11 +1,18 @@
+import platform
 import urllib
 import os
 
-bucketURL = "https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core/libledger-core.so"
-libFile = "lib/libledger-core.so"
+baseURL = "https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
+userPlatform = platform.system()
+
+bucketFile = "libledger-core.so"
+if userPlatform == "Darwin":
+    bucketFile = "libledger-core.dylib"
+else if userPlatform == "Windows":
+    bucketFile = "libledger-core.dll"
 
 opener = urllib.URLopener()
 
 print('Downloading ' + bucketURL + ' ...')
-opener.retrieve(bucketURL, libFile)
+opener.retrieve(baseURL + "/" + bucketURL, "lib/" + libFile)
 os.chmod(libFile, 0755)
