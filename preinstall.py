0a1d404130e7386b49745be9f80783606c85eaf6
meriadec <meriadec.pillet@gmail.com>
2018-05-11 10:31:58 +0200

    fix else, throw if unsupported platform

--- preinstall.py
+++ preinstall.py
@@ -5,14 +5,21 @@ import os
 baseURL = "https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
 userPlatform = platform.system()
 
-bucketFile = "libledger-core.so"
-if userPlatform == "Darwin":
+bucketFile = ""
+if userPlatform == "Linux":
+    bucketFile = "libledger-core.so"
+elif userPlatform == "Darwin":
     bucketFile = "libledger-core.dylib"
-else if userPlatform == "Windows":
+elif userPlatform == "Windows":
     bucketFile = "libledger-core.dll"
+else:
+    raise Exception('Unsupported platform ' + userPlatform)
 
 opener = urllib.URLopener()
 
-print('Downloading ' + bucketURL + ' ...')
-opener.retrieve(baseURL + "/" + bucketURL, "lib/" + libFile)
-os.chmod(libFile, 0755)
+endpointURL = baseURL + "/" + bucketFile
+finalFile = "lib/" + bucketFile
+
+print('Downloading ' + endpointURL + '...')
+opener.retrieve(endpointURL, finalFile)
+os.chmod(finalFile, 0755)
