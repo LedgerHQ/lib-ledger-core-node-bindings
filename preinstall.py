e11c08bcbec807a29756ce3a4fd4e3ddfb77279f
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2018-09-10 17:25:26 +0200

    Bump libcore to 1.1.0

--- preinstall.py
+++ preinstall.py
@@ -2,7 +2,7 @@ import platform
 import urllib
 import os
 
-libCoreVersion = "1.0.1"
+libCoreVersion = "1.1.0"
 baseURL = "https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
 filePath = ""
 
