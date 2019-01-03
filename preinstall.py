22c8b5e41157df6a2a1a8b545e5fea276a39daf2
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2019-01-03 23:32:59 +0100

    Bump libcore to 1.1.8: support for XSN and activate Sapling for Komodo

--- preinstall.py
+++ preinstall.py
@@ -2,7 +2,7 @@ import platform
 import urllib
 import os
 
-libCoreVersion = "1.1.7"
+libCoreVersion = "1.1.8"
 baseURL = "https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
 filePath = ""
 
