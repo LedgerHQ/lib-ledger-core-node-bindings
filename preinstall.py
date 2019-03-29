af77620ff5da9c1bb81aa6298285d422e95a0dc5
KhalilBellakrid <khalil.bellakrid@ledger.fr>
2019-03-29 11:16:26 +0100

    Bump libcore: fix on bech32 and Android crash

--- preinstall.py
+++ preinstall.py
@@ -2,7 +2,7 @@ import platform
 import urllib
 import os
 
-libCoreVersion = "2.7.0-rc-43d760"
+libCoreVersion = "2.7.0-rc-f356d0"
 
 baseURL = "https://s3-eu-west-1.amazonaws.com/ledger-lib-ledger-core"
 filePath = ""
